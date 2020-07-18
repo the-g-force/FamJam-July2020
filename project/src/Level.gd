221b80db2dafcd2b59e36e15614fd875378d67c8
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-07-18 10:09:02 -0400

    Add obstacle generation

--- project/src/Level.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-onready var _obstacletimer : Timer = $Timer
-
-var _obstacles := []
-
-
-func _on_Timer_timeout():
-	pass
---
+++ project/src/Level/Level.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+onready var _obstacletimer : Timer = $Timer
+
+export var slidespeed := 10.0
+
+var screensize := Vector2(0,0)
+
+func _ready():
+	screensize = get_viewport_rect().size
+
+func _physics_process(_delta):
+	var _moving_nodes = get_tree().get_nodes_in_group("Moving")
+	for object in _moving_nodes:
+		if object.position.x > 0:
+			object.position.x -= slidespeed
+		else:
+			object.queue_free()
+
+
+func _on_Timer_timeout():
+	var _type = 1#randi()%2
+	var obstacle
+	var Obstacle
+	if _type == 1:
+		obstacle = load("res://src/Obstacles/GroundThing.tscn")
+		Obstacle = obstacle.instance()
+		Obstacle.position = Vector2(screensize.x, 200)
+	add_child(Obstacle)
+	slidespeed += 1
