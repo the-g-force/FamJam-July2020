c79a23876b14f80655286031e2c8b9a4ed761aa8
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-07-18 10:31:17 -0400

    Add flying obstacles

--- project/src/Level/Level.gd
+++ project/src/Level/Level.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 onready var _obstacletimer : Timer = $Timer
 
-export var slidespeed := 10.0
+export var slidespeed := 15.0
 
 var screensize := Vector2(0,0)
 
@@ -19,12 +19,19 @@ func _physics_process(_delta):
 
 
 func _on_Timer_timeout():
-	var _type = 1#randi()%2
-	var obstacle
-	var Obstacle
+	randomize()
+	var _type = randi()%2
 	if _type == 1:
+		var obstacle
 		obstacle = load("res://src/Obstacles/GroundThing.tscn")
+		var Obstacle
 		Obstacle = obstacle.instance()
 		Obstacle.position = Vector2(screensize.x, 400)
-	add_child(Obstacle)
-	slidespeed += 1
+		add_child(Obstacle)
+	elif _type == 0:
+		var obstacle
+		obstacle = load("res://src/Obstacles/FlyingThing.tscn")
+		var Obstacle
+		Obstacle = obstacle.instance()
+		Obstacle.position = Vector2(screensize.x, 200)
+		add_child(Obstacle)
