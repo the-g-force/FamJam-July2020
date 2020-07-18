952d2a7e0e25eee5c7eeb239529d222fd8e3f4af
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-07-18 15:39:41 -0400

    Switch player jump to animation player

--- project/src/Player/Player.gd
+++ project/src/Player/Player.gd
@@ -1,16 +1,11 @@
 class_name Player
 extends Node2D
 
-onready var _actiontimer : Timer = $Actiontimer
+onready var _jump : AnimationPlayer = $AnimationPlayer
 onready var _cutout : Cutout = $CutoutAnimation
-
-export var _jumptime := 0.2
-export var _ducktime := 0.4
-export var _jumpspeed := 10
 
 var _jumping := false
 var _ducking := false
-var _up := false
 var _playing := true
 
 signal end_game
@@ -22,25 +17,7 @@ func _physics_process(delta):
 			_cutout.duck()
 		elif Input.is_action_just_pressed("jump") and not _ducking and not _jumping:
 			_jumping = true
-			_up = true
-			_actiontimer.start(_jumptime)
-		var _velocity := Vector2(0,0)
-		if _up:
-			_velocity.y -= 1
-		elif _jumping and not _up:
-			_velocity.y += 1
-		_velocity *= _jumpspeed*delta
-		position += _velocity
-
-
-func _on_Actiontimer_timeout():
-	if _playing:
-		if _jumping and _up:
-			_up = false
-			_actiontimer.start(_jumptime)
-		elif _jumping and not _up:
-			_jumping = false
-			position = Vector2(100,500)
+			_jump.play("Jump")
 
 
 func _on_CutoutAnimation_hit_something(area):
@@ -52,3 +29,7 @@ func _on_CutoutAnimation_hit_something(area):
 
 func _on_CutoutAnimation_duck_over():
 	_ducking = false
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	_jumping = false
