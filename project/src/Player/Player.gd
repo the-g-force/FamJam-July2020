7dfddccc9323808f10662cf85f73f584a99b6be3
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-07-18 12:52:50 -0400

    Prevent duck and jump input after game over

--- project/src/Player/Player.gd
+++ project/src/Player/Player.gd
@@ -11,36 +11,40 @@ export var _jumpspeed := 10
 var _jumping := false
 var _ducking := false
 var _up := false
+var _playing := true
 
 signal end_game
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("duck") and not _ducking and not _jumping:
-		_ducking = true
-		_cutout.duck()
-	elif Input.is_action_just_pressed("jump") and not _ducking and not _jumping:
-		_jumping = true
-		_up = true
-		_actiontimer.start(_jumptime)
-	var _velocity := Vector2(0,0)
-	if _up:
-		_velocity.y -= 1
-	elif _jumping and not _up:
-		_velocity.y += 1
-	_velocity *= _jumpspeed*delta
-	position += _velocity
+	if _playing:
+		if Input.is_action_just_pressed("duck") and not _ducking and not _jumping:
+			_ducking = true
+			_cutout.duck()
+		elif Input.is_action_just_pressed("jump") and not _ducking and not _jumping:
+			_jumping = true
+			_up = true
+			_actiontimer.start(_jumptime)
+		var _velocity := Vector2(0,0)
+		if _up:
+			_velocity.y -= 1
+		elif _jumping and not _up:
+			_velocity.y += 1
+		_velocity *= _jumpspeed*delta
+		position += _velocity
 
 
 func _on_Actiontimer_timeout():
-	if _jumping and _up:
-		_up = false
-		_actiontimer.start(_jumptime)
-	elif _jumping and not _up:
-		_jumping = false
+	if _playing:
+		if _jumping and _up:
+			_up = false
+			_actiontimer.start(_jumptime)
+		elif _jumping and not _up:
+			_jumping = false
 
 
 func _on_CutoutAnimation_hit_something(area):
 	if area is Obstacle:
+		_playing = false
 		emit_signal("end_game")
 
 
