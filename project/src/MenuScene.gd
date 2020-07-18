f190c538e16ad8f7691c7ac21dc72d7a5c989caf
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-07-18 14:41:33 -0400

    Add music toggle stay toggled when scene reloaded

--- project/src/MenuScene.gd
+++ project/src/MenuScene.gd
@@ -2,6 +2,10 @@ extends Control
 
 onready var _MUSIC_BUS : int = AudioServer.get_bus_index("Music")
 onready var _Level = preload("res://src/Level/Level.tscn")
+onready var _musicmutetoggle = $MusicMuteToggle
+
+func _ready():
+	_musicmutetoggle.pressed = false if AudioServer.is_bus_mute(_MUSIC_BUS) else true
 
 func _on_Button_pressed():
 	var _ignoredResult = get_tree().change_scene_to(_Level)
