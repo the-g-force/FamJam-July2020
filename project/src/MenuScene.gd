ec968eeb82be36e56f8e0bbaad81edb2c729c943
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-07-18 12:01:36 -0400

    Add mutable music bus

--- project/src/MenuScene.gd
+++ project/src/MenuScene.gd
@@ -1,6 +1,11 @@
 extends Control
 
+onready var _MUSIC_BUS : int = AudioServer.get_bus_index("Music")
 onready var _Level = preload("res://src/Level/Level.tscn")
 
 func _on_Button_pressed():
 	var _ignoredResult = get_tree().change_scene_to(_Level)
+
+
+func _on_MusicMuteToggle_toggled(button_pressed):
+	AudioServer.set_bus_mute(_MUSIC_BUS, not button_pressed)
